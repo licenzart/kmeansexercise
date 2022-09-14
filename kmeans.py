583ab51a46c7b509cb0bb5db80e2bac3ccc7d35f
licenzart <109023681+licenzart@users.noreply.github.com>
2022-09-14 11:15:29 +0800

    Add files via upload

---
+++ kmeans.py
@@ -0,0 +1,33 @@
+#Import Packages
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+file = "mall_customer.csv"
+df = pd.read_csv(file)
+
+st.header("My first Streamlit App for Mall Dataset")
+
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['whole data', 'description','KMeans cluster'])
+
+if option=='whole data':
+    whole = df
+    st.write(whole)
+    
+if option=='description':
+    chart_data = df.describe()
+    st.write(chart_data)
+
+else:
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(X)
+    y_kmeans = kmeans.predict(X)
+    plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+    centers = kmeans.cluster_centers_
+    KMeans_data = plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
+    st.write(KMeans_data)
