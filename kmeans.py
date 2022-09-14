a3379e1dfb66412e201ad724b7db108ee916fcfc
licenzart <109023681+licenzart@users.noreply.github.com>
2022-09-14 12:01:16 +0800

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -34,5 +34,5 @@ else:
     y_kmeans = kmeans.predict(X)
     plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
     centers = kmeans.cluster_centers_
-    plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
-    
+    plot = plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
+    st.write(plot)
